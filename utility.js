5d697eb88cd5c78e0f29c5ad44b5e5728be6fd5a
Aditya Kotwal <75358971+Ad999s@users.noreply.github.com>
2022-12-12 12:51:37 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,11 +1,11 @@
 let utility = {
-    media: ['mp4', 'mkv', 'png', 'jpg', 'JPG', 'jpeg', 'mov', 'MOV', 'mpg'],
+    media: ['mp4', 'mkv', 'png', 'PNG', 'FIFAMOD', 'jpg', 'JPG', 'jpeg', 'mov', 'MOV', 'mpg'],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
     documents: ['xml', 'md', 'ppt', 'pptx', 'csv',
         'docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'odp', 'odg', 'odf', 'txt', 'ps'
     ],
-    app: ['exe', 'dmg', 'pkg', 'deb', 'AppImage'],
-    code: ['c', 'py', 'js', 'out',
+    app: ['exe', 'fifamod', 'dmg', 'pkg', 'deb', 'AppImage'],
+    code: ['c', 'py', 'jfif', 'psd', 'js', 'out',
         'cpp', 'html', 'css', 'yml', 'json', 'ipynb', 'sh', 'db', 'avif'
     ],
     chess: ['pgn']
