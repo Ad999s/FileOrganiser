4769b00198dcea9168f2286d793203199849ca7d
Aditya Kotwal <75358971+Ad999s@users.noreply.github.com>
2022-12-12 12:52:06 +0530

    Update mainn.js

--- mainn.js
+++ mainn.js
@@ -3,6 +3,8 @@
 // organise "dirpath"->organises given directory
 // help ->list commands 
 let fs = require("fs");
+const fse = require('fs-extra');
+const { dirname } = require("path");
 let path = require("path");
 let u = require("./utility.js");
 let inputArr = process.argv.slice(2);
@@ -45,23 +47,19 @@ function organise(dirpath) {
         console.log("Please enter valid path");
         return;
     }
+
 }
 
 function organiseHelper(dirpath) {
+    // dirpath defines directory to be organised
     let dir = "Organised";
     let destPath = path.join(dirpath, dir);
-
+    // edge case that checks that organised folder is already organised or not
     if (fs.existsSync(destPath) == false) {
-
         fs.mkdirSync(destPath);
-
     }
-    // lets create subfolder in destpath
-    for (let key in u) {
-        let temp = path.join(destPath, key);
-        if (fs.existsSync(temp) == false)
-            fs.mkdirSync(temp);
-    }
+    // lets create subfolders in destpath
+    createSubf(destPath);
     // now we need to loop in source directory for files and start copying in destpath
     let contents = fs.readdirSync(dirpath);
     if (contents.length == 0) {
@@ -70,28 +68,89 @@ function organiseHelper(dirpath) {
     let filescopied = 0;
     for (let i = 0; i < contents.length; i++) {
         // need to get extension for content
-        let pathDetails = fs.lstatSync(dirpath + "\/" + contents[i]);
-        let isfile = pathDetails.isFile();
-        let ext = path.extname(contents[i]);
-        let copyTo;
-        if (isfile)
-            for (let folder in u) {
-                // console.log(folder + "\n");
-                for (let j = 0; j < u[folder].length; j++) {
-                    if (ext == "." + u[folder][j]) {
-                        // copy that file to destination folder
-                        let filepath = path.join(dirpath, contents[i]);
-                        let copyTo = path.join(destPath, folder);
-                        fs.copyFile(filepath, copyTo + "\/" + contents[i], (err) => {
-                            if (err)
-                                throw err;
-                            filescopied++;
-                            console.log(filescopied, "out of", contents.length - 1);
-                            fs.unlinkSync(filepath);
-                        });
+        console.log(++filescopied, "out of", contents.length)
+        if (contents[i] != dir) {
 
+            let content = path.join(dirpath, contents[i]);
+            let pathDetails = fs.lstatSync(content);
+            let isfile = pathDetails.isFile();
+            let ext = path.extname(contents[i]);
+            // if ()
+            let copyTo;
+            if (isfile) {
+                for (let folder in u) {
+                    for (let j = 0; j < u[folder].length; j++) {
+                        if (ext == "." + u[folder][j]) {
+                            // copy that file to destination folder
+                            let filepath = path.join(dirpath, contents[i]);
+                            let copyTo = path.join(destPath, folder);
+                            fs.copyFileSync(filepath, copyTo + "\/" + contents[i], (err) => {
+                                if (err)
+                                    throw err;
+                                fs.unlinkSync(filepath);
+                            });
+
+                        }
                     }
                 }
+            } else {
+                let source = path.join(dirpath, contents[i]);
+                let destination = path.join(destPath, "Folders", contents[i]);
+                console.log("Copying Subfolder ", contents[i]);
+                copyfolder(source, destination, contents[i], 1);
+                console.log("Done copying ", contents[i]);
+                console.log("\n");
+
             }
+
+        }
+    }
+
+}
+
+function createSubf(destPath) {
+    for (let subF in u) {
+        let temp = path.join(destPath, subF);
+        if (fs.existsSync(temp) == false)
+            fs.mkdirSync(temp);
+    }
+    let temp = path.join(destPath, "Folders");
+    if (fs.existsSync(temp) == false)
+        fs.mkdirSync(temp);
+}
+
+
+function copyfolder(src, dest, dirName, k) {
+    // creating folder named dirname in destination folder
+    // console.log(1);
+    if (fs.existsSync(dest) == true) {
+        return;
+    }
+    fs.mkdirSync(dest);
+    // loop through src folder
+    let contents = fs.readdirSync(src);
+    //base case
+    if (contents.length == 0) {
+        return;
+    }
+    // getting contents in particular directory given
+    for (let i = 0; i < contents.length; i++) {
+        let subf = path.join(src, contents[i]);
+        let pathDetails = fs.lstatSync(subf);
+        let isfile = pathDetails.isFile();
+        if (!isfile) {
+            // create a folder called contents[i] in destination folder
+            let fol = path.join(dest, contents[i]);
+            // copy recursively but now destination changes
+            copyfolder(subf, fol, contents[i], ++k);
+
+
+        } else {
+            let filepath = path.join(src, contents[i]);
+            fs.copyFileSync(filepath, dest + "\/" + contents[i], (err) => {
+                if (err)
+                    throw err;
+            });
+        }
     }
 }
